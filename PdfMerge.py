65802451fa9d4919066a2fc25b4bf153531c16df
krysx7003 <krysx7003@gmail.com>
2025-03-24 22:22:55 +0100

    Lab1 var7

--- PdfMerge.py
+++ PdfMerge.py
@@ -4,11 +4,8 @@ import pdfkit
 
 # Get the path of the notebook directory
 notebook_dir = os.path.dirname(os.path.abspath(__file__))
-dir = input() 
+dir = input(">>") 
 target_path = os.path.join(notebook_dir, dir)
-
-# Create a new PDF file object
-merged_pdf = PdfMerger()
 
 for file_name in os.listdir(target_path):
     if file_name.endswith('.html'):
@@ -16,7 +13,10 @@ for file_name in os.listdir(target_path):
         pdf_path = os.path.join(target_path,"Sprawozdanie", file_name.removesuffix(".html") + ".pdf") 
         pdfkit.from_file(html_path,pdf_path)
 
+# Create a new PDF file object
 target_path = os.path.join(notebook_dir, dir, "Sprawozdanie")
+merged_pdf = PdfMerger()
+
 # Loop through all PDF files in the notebook directory
 for file_name in os.listdir(target_path):
     if file_name.endswith('.pdf'):
@@ -24,5 +24,5 @@ for file_name in os.listdir(target_path):
         merged_pdf.append(open(os.path.join(target_path, file_name), 'rb'))
 
 # Save the merged PDF file
-with open('Lab1/Sprawozdanie/Sprawozdanie_1.pdf', 'wb') as output_file:
+with open(dir+'/Sprawozdanie/Sprawozdanie_1.pdf', 'wb') as output_file:
     merged_pdf.write(output_file)
