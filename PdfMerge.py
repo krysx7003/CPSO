3ec2b26d3522da11a9186c1e20a1536b63303e7f
krysx7003 <krysx7003@gmail.com>
2025-03-24 22:31:48 +0100

    Lab1 done

--- PdfMerge.py
+++ PdfMerge.py
@@ -4,25 +4,28 @@ import pdfkit
 
 # Get the path of the notebook directory
 notebook_dir = os.path.dirname(os.path.abspath(__file__))
-dir = input(">>") 
-target_path = os.path.join(notebook_dir, dir)
+dir = input(">> ") 
+target_path = os.path.join(notebook_dir, dir,"Sprawozdanie")
 
 for file_name in os.listdir(target_path):
     if file_name.endswith('.html'):
         html_path = os.path.join(target_path, file_name) 
-        pdf_path = os.path.join(target_path,"Sprawozdanie", file_name.removesuffix(".html") + ".pdf") 
+        pdf_path = os.path.join(target_path, file_name.removesuffix(".html") + ".pdf") 
         pdfkit.from_file(html_path,pdf_path)
 
 # Create a new PDF file object
-target_path = os.path.join(notebook_dir, dir, "Sprawozdanie")
 merged_pdf = PdfMerger()
 
 # Loop through all PDF files in the notebook directory
-for file_name in os.listdir(target_path):
+pdf_files = sorted(
+    [f for f in os.listdir(target_path) if f.endswith(".pdf")],
+    key=lambda x: int("".join(filter(str.isdigit, x)))  # Extracts numbers for sorting
+)
+for file_name in pdf_files:
     if file_name.endswith('.pdf'):
         # Add the PDF file to the merger object
         merged_pdf.append(open(os.path.join(target_path, file_name), 'rb'))
 
 # Save the merged PDF file
-with open(dir+'/Sprawozdanie/Sprawozdanie_1.pdf', 'wb') as output_file:
+with open(dir+'/Sprawozdanie_1.pdf', 'wb') as output_file:
     merged_pdf.write(output_file)
